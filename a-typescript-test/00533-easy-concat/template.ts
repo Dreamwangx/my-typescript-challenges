18e27a763225028736d7ad068fe2fa1905ba0b48
wangxin0 <wangin0@egova.com.cn>
2024-03-22 22:00:59 +0800

    533 - concat --> unknow[]

--- a-typescript-test/00533-easy-concat/template.ts
+++ a-typescript-test/00533-easy-concat/template.ts
@@ -1,1 +1,7 @@
-type Concat<T, U> = any
+type Concat< T extends  readonly [ ...any], U extends readonly [...any]> = [
+    ...T,...U
+]
+type Concat2< T extends  readonly unknown[], U extends readonly unknown[]> = [
+    ...T,...U
+]
+type c = Concat<[1], [1,3]>

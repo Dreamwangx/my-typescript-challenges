55ee4b1277e7c37bc3d08006e6c077196f2b943a
wangxin0 <wangin0@egova.com.cn>
2024-03-22 22:03:07 +0800

    189 - awaited ---> PromiseLike  infer

--- a-typescript-test/00189-easy-awaited/template.ts
+++ a-typescript-test/00189-easy-awaited/template.ts
@@ -1,1 +1,10 @@
-type MyAwaited<T> = any
+//type MyAwaited<T> = T extends Promise<infer X> ? MyAwaited<X> : T
+
+type MyAwaited<T extends PromiseLike<any>> = 
+T extends PromiseLike<infer R> 
+    ? R extends PromiseLike<any> 
+        ? MyAwaited<R> : R
+    : never
+
+// 上面泛型约束 ,不是PromiseLike 类型提示报错
+//type MyAwaited<T> = T extends PromiseLike<infer R> ? MyAwaited<R> : T

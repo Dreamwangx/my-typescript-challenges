88d35ae55bbbd6237c4f0fb125b52103001060e1
wangxin0 <wangin0@egova.com.cn>
2024-03-24 19:00:15 +0800

    00003-Omit-->Exclude

--- a-typescript-test/00003-medium-omit/template.ts
+++ a-typescript-test/00003-medium-omit/template.ts
@@ -1,1 +1,14 @@
-type MyOmit<T, K> = any
+type MyOmit<T, K extends keyof T> =  
+{ 
+    [P in Exclude<keyof T,K>] : T[P]
+}
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+type CC  = MyOmit<Todo, 'description'>
+
+type CC2 = Todo
